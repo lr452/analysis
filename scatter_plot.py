b68f203676892a71861e7628a5e3834e9d11b0ed
Lily Roberts <lr452@mail.example.com>
2021-02-16 15:39:08 +0000

    Added in model data and wind text data

--- scatter_plot.py
+++ scatter_plot.py
@@ -1,1 +1,52 @@
 import iris 
+import numpy as np
+import iris.quickplot as qplt
+import matplotlib.pyplot as plt
+from iris.coord_categorisation import *
+from matplotlib.pyplot import *
+import matplotlib as mpl
+#import cartopy.crs as ccrs
+#import cartopy.feature as cfeature
+
+
+#Load in model data
+cube1 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_ACCESS-ESM1-5_historical_r1i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+cube2 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_CanESM5_historical_r1i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+cube3 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_CESM2_historical_r1i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+cube4 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_GFDL-CM4_historical_r1i1p1f1_gr_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+cube5 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_GISS-E2-1-G_historical_r101i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+cube6 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_IPSL-CM6A-LR_historical_r1i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+cube7 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_MIROC-ES2L_historical_r1i1p1f2_gn_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+cube8 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_MPI-ESM1-2-LR_historical_r1i1p1f1_gn_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+cube9 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_NorESM2-MM_historical_r1i1p1f1_gr_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+cube10 = iris.load_cube('/disk2/lr452/Downloads/dissic_data/dissic_Omon_UKESM1-0-LL_historical_r1i1p1f2_gn_199401-201412.rg.yr.so.fix.mask.nc','dissic')
+
+####### IS THERE A WAY TO DO THIS IN A FOR LOOP?
+#Taking the mean value across the 20 years
+average_across_time = cube.collapsed(['time'],iris.analysis.MEAN)
+
+#Averaging across depth
+max_depth = 100.0
+indexes = np.where(average_across_time.coord('lev').points <= max_depth)[0] #This would create error in for loop as each model names the depth variable something different
+average_across_time = average_across_time[indexes]
+average_across_depth = average_across_time.collapsed(['lev'],iris.analysis.MEAN)
+
+#Averaging across longitude
+cube_region.coord('latitude').guess_bounds()
+cube_region.coord('longitude').guess_bounds()
+grid_areas = iris.analysis.cartography.area_weights(cube_region)
+global_average_variable = cube_region.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
+
+#Take the difference in DIC concentration between 40-65 and divide by mean value
+
+#######
+
+#Load wind data
+wind_variable = [51.37, 50.68, 53.2, 50.63, 52.2, 50.47, 46.11, 49.16, 53.81, 51.78]
+
+
+variable1 = #DIC concentration
+variable2 = wind_variable 
+
+plt.scatter(variable1, variable2)
+plt.show 
