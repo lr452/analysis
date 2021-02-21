13195cfb73e00f5df753c4e829c17ff79c178145
Lily Roberts <lr452@mail.example.com>
2021-02-21 14:29:26 +0000

    Working

--- scatter_plot.py
+++ scatter_plot.py
@@ -37,12 +37,12 @@ average_across_depth1.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth1)
 cube1_average = average_across_depth1.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
 
-print(cube1_average)
-print(cube1_average.coord('latitude').points)
+#print(cube1_average)
+#print(cube1_average.coord('latitude').points)
 
-cube1_n = cube1_average[37:39].collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
-cube1_s = cube1_average[13:15].collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
-cube1_av = cube1_average[:].collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube1_n = np.mean(cube1_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube1_s = np.mean(cube1_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube1_av = np.mean(cube1_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
 cube1_dic = (cube1_n - cube1_s) / cube1_av
 print(cube1_dic) 
 
@@ -61,6 +61,14 @@ average_across_depth2.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth2)
 cube2_average = average_across_depth2.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
 
+#print(cube2_average.coord('latitude').points)
+
+cube2_n = np.mean(cube2_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube2_s = np.mean(cube2_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube2_av = np.mean(cube2_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube2_dic = (cube2_n - cube2_s) / cube2_av
+print(cube2_dic) 
+
 ###CUBE 3
 #Time average, depth average and longitude average 
 average_across_time3 = cube3.collapsed(['time'],iris.analysis.MEAN)
@@ -74,6 +82,14 @@ average_across_depth3.coord('latitude').guess_bounds()
 average_across_depth3.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth3)
 cube3_average = average_across_depth3.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
+
+#print(cube3_average.coord('latitude').points)
+
+cube3_n = np.mean(cube3_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube3_s = np.mean(cube3_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube3_av = np.mean(cube3_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube3_dic = (cube3_n - cube3_s) / cube3_av
+print(cube3_dic)
 
 ###CUBE 4
 #Time average, depth average and longitude average 
@@ -89,6 +105,14 @@ average_across_depth4.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth4)
 cube4_average = average_across_depth4.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
 
+#print(cube4_average.coord('latitude').points)
+
+cube4_n = np.mean(cube4_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube4_s = np.mean(cube4_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube4_av = np.mean(cube4_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube4_dic = (cube4_n - cube4_s) / cube4_av
+print(cube4_dic)
+
 ###CUBE 5
 #Time average, depth average and longitude average 
 average_across_time5 = cube5.collapsed(['time'],iris.analysis.MEAN)
@@ -102,6 +126,14 @@ average_across_depth5.coord('latitude').guess_bounds()
 average_across_depth5.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth5)
 cube5_average = average_across_depth5.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
+
+#print(cube5_average.coord('latitude').points)
+
+cube5_n = np.mean(cube5_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube5_s = np.mean(cube5_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube5_av = np.mean(cube5_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube5_dic = (cube5_n - cube5_s) / cube5_av
+print(cube5_dic)
 
 ###CUBE 6
 #Time average, depth average and longitude average 
@@ -117,6 +149,15 @@ average_across_depth6.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth6)
 cube6_average = average_across_depth6.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
 
+#print(cube6_average.coord('latitude').points)
+
+cube6_n = np.mean(cube6_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube6_s = np.mean(cube6_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube6_av = np.mean(cube6_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube6_dic = (cube6_n - cube6_s) / cube6_av
+print(cube6_dic)
+
+
 ###CUBE 7
 #Time average, depth average and longitude average 
 average_across_time7 = cube7.collapsed(['time'],iris.analysis.MEAN)
@@ -131,6 +172,14 @@ average_across_depth7.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth7)
 cube7_average = average_across_depth7.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
 
+#print(cube7_average.coord('latitude').points)
+
+cube7_n = np.mean(cube7_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube7_s = np.mean(cube7_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube7_av = np.mean(cube7_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube7_dic = (cube7_n - cube7_s) / cube7_av
+print(cube7_dic)
+
 ###CUBE 8
 #Time average, depth average and longitude average 
 average_across_time8 = cube8.collapsed(['time'],iris.analysis.MEAN)
@@ -144,6 +193,14 @@ average_across_depth8.coord('latitude').guess_bounds()
 average_across_depth8.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth8)
 cube8_average = average_across_depth8.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
+
+#print(cube8_average.coord('latitude').points)
+
+cube8_n = np.mean(cube8_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube8_s = np.mean(cube8_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube8_av = np.mean(cube8_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube8_dic = (cube8_n - cube8_s) / cube8_av
+print(cube8_dic)
 
 
 ###CUBE 9
@@ -160,6 +217,14 @@ average_across_depth9.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth9)
 cube9_average = average_across_depth9.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
 
+#print(cube9_average.coord('latitude').points)
+
+cube9_n = np.mean(cube9_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube9_s = np.mean(cube9_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube9_av = np.mean(cube9_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube9_dic = (cube9_n - cube9_s) / cube9_av
+print(cube9_dic)
+
 ###CUBE 10
 #Time average, depth average and longitude average 
 average_across_time10 = cube10.collapsed(['time'],iris.analysis.MEAN)
@@ -174,18 +239,22 @@ average_across_depth10.coord('longitude').guess_bounds()
 grid_areas = iris.analysis.cartography.area_weights(average_across_depth10)
 cube10_average = average_across_depth10.collapsed(['longitude'],iris.analysis.MEAN,weights=grid_areas)
 
+#print(cube10_average.coord('latitude').points)
 
+cube10_n = np.mean(cube10_average.data[37:39])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube10_s = np.mean(cube10_average.data[13:15])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube10_av = np.mean(cube10_average.data[:])#.collapsed(['latitude'],iris.analysis.MEAN,weights=grid_areas)
+cube10_dic = (cube10_n - cube10_s) / cube10_av
+print(cube10_dic)
 
-#Take the difference in DIC concentration between 40-65 and divide by mean value
-
-#######
 
 #Load wind data
-#wind_variable = [51.37, 50.68, 53.2, 50.63, 52.2, 50.47, 46.11, 49.16, 53.81, 51.78]
+wind_variable = [51.37, 50.68, 53.2, 50.63, 52.2, 50.47, 46.11, 49.16, 53.81, 51.78]
 
 
-#variable1 = #DIC concentration
-#variable2 = wind_variable 
+variable1 = [cube1_dic, cube2_dic, cube3_dic, cube4_dic, cube5_dic, cube6_dic, cube7_dic, cube8_dic, cube9_dic, cube10_dic]
+variable2 = wind_variable 
 
-#plt.scatter(variable1, variable2)
-#plt.show 
+plt.scatter(variable2, variable1)
+plt.legend()
+plt.show()
